483517aa57a54efc655a62fd701423a278cf1b29
Rawksawlid <38799629+Rawksawlid@users.noreply.github.com>
2018-04-27 20:06:16 -0500

    Full Game Current
    Delete Later

---
+++ FirstProjectForFun/Assets/Scripts/BattleSystem/EnemyStateMachine.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStateMachine : MonoBehaviour {
+
+    // Calling base enemy to use it
+    public BaseEnemy enemy;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
